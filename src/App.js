db5ca6acd601c210e13829803605bd8a34e54899
Pratibhapa <pratibhaa4321@gmail.com>
2023-10-18 12:14:44 +0545

    update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
+import { clear } from '@testing-library/user-event/dist/clear';
 
 const App = () => {
   const [result, setResult] = useState("");
 
   const handleClick = (e) => {
-    setResult(e.target.name);
+    setResult(result.concat(e.target.name));
+  }
+  const clear = () => {
+    setResult("");
+  }
+  const backspace = () => {
+    setResult(result.slice(0, result.length - 1));
+  }
+  const calculate = () => {
+    try{
+      setResult(eval(result).toString());
+    } catch(err) {
+      setResult("Error")
+    }
   }
   return (
     <>
@@ -15,24 +29,24 @@ const App = () => {
         <input type='text' value={result}/>
       </form>
       <div className='keypad'>
-        <button onClick={handleClick}>Clear</button>
-        <button onClick={handleClick}>C</button>
-        <button onClick={handleClick}>&divide;</button>
-        <button onClick={handleClick}>7</button>
-        <button onClick={handleClick}>8</button>
-        <button onClick={handleClick}>9</button>
-        <button onClick={handleClick}>&times;</button>
-        <button onClick={handleClick}>4</button>
-        <button onClick={handleClick}>5</button>
-        <button onClick={handleClick}>6</button>
-        <button onClick={handleClick}>&ndash;</button>
-        <button onClick={handleClick}>1</button>
-        <button onClick={handleClick}>2</button>
-        <button onClick={handleClick}>3</button>
-        <button onClick={handleClick}>+</button>
-        <button onClick={handleClick}>0</button>
-        <button onClick={handleClick}>.</button>
-        <button onClick={handleClick}>=</button>
+        <button className='highlight' onClick={clear} id='clear'>Clear</button>
+        <button className='highlight' onClick={backspace} id='backspace'>C</button>
+        <button name='/' onClick={handleClick}>&divide;</button>
+        <button name='7' onClick={handleClick}>7</button>
+        <button name='8' onClick={handleClick}>8</button>
+        <button name='9' onClick={handleClick}>9</button>
+        <button className='highlight' name='*' onClick={handleClick}>&times;</button>
+        <button name='4' onClick={handleClick}>4</button>
+        <button name='5' onClick={handleClick}>5</button>
+        <button name='6' onClick={handleClick}>6</button>
+        <button className='highlight' name='-' onClick={handleClick}>&ndash;</button>
+        <button name='1' onClick={handleClick}>1</button>
+        <button name='2' onClick={handleClick}>2</button>
+        <button name='3' onClick={handleClick}>3</button>
+        <button className='highlight' name='+' onClick={handleClick}>+</button>
+        <button name='0' onClick={handleClick}>0</button>
+        <button name='.' onClick={handleClick}>.</button>
+        <button className='highlight' onClick={calculate}>=</button>
       </div>
     </div>
     </>
